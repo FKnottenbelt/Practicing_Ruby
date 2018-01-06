13bf4de2457866c0047cddf45ee5abfe3cb9c7c6
Femke Knottenbelt <femknotgithub@gmail.com>
2018-01-06 12:10:43 +0000

    chapter 10 added Dictionary sort excercise

--- cpine/chap_10nw.rb
+++ cpine/chap_10nw.rb
@@ -95,3 +95,56 @@ end
 
 print shuffle(original_array)
 puts
+#####################################################
+###############################################
+# Sort by seeing which word comes first in the dictionary, and pushing 
+# that to the end of a 'sorted_array' while also making a list of unsorted words.
+# And don't use .sort except as 'right answer' check
+# But now with recursion. And comparing lowercase, but showing original case
+
+original_array = ["noot","Aap","Mies","aap","noot"]
+work_array = [].replace(original_array) #to make a real independent copy
+array_with_sort = original_array.sort
+
+sorted_array=[]
+
+def find_smallest work_array, sorted_array
+  #base case
+  if work_array.empty?
+    return sorted_array #done, nothing more to sort
+  end
+  
+  #do the work
+  smallest="zzzzz"
+  work_array.each do
+    |word| if word.downcase < smallest.downcase
+             smallest = word
+           end
+  end
+  sorted_array.push(smallest)
+  work_array.delete_at(work_array.index(smallest)) #work towards base
+  find_smallest(work_array, sorted_array) #recursion
+end
+
+find_smallest(work_array, sorted_array)
+
+puts "original_array #{original_array}"
+print "'hand' sorted array: #{sorted_array}"
+puts
+puts "with sort: #{array_with_sort}"
+##############################################
+# And again, this time using quicksort 
+original_array = ["noot","Aap","Mies","aap","zoro","moot"]
+
+def sort arr
+  return arr if arr.length <= 1 # base case
+
+  middle = arr.pop  # degene met welke je vergelijkt 
+  less = arr.select{ |x| x.downcase < middle.downcase} # kleiner dan 'vergelijk item'
+  more = arr.select{ |x| x.downcase >= middle.downcase} #groter dan 'vergelijk item'
+  sort(less) + [middle] + sort(more) #verdeel en heers! middle komt terug, less
+                                     # en more gaan dieper.
+end
+
+print sort (original_array)
+puts
