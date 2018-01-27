9419220c30b448b2815187289a54a9131c2daaed
Femke Knottenbelt <femknotgithub@gmail.com>
2018-01-27 18:07:05 +0000

    Add lssg_arr1 problem

---
+++ lssg_problems/lssg_arr1.rb
@@ -0,0 +1,34 @@
+#The following array contains three names and numbers. 
+# Group each name with the number following it by placing 
+# the pair in their own array. Then create a nested array 
+# containing all three groups. 
+
+# arr = ['Dave', 7, 'Miranda', 3, 'Jason', 11]
+
+# expected output:
+# [['Dave', 7],['Miranda', 3],['Jason', 11]]
+
+#=== my solution ===
+
+arr =['Dave', 7, 'Miranda', 3, 'Jason', 11]
+
+new_arr=[]
+new_sub_arr = []
+
+
+arr.each_with_index do |e, i|
+
+  if i.even?
+    # start new sub arr
+    new_arr.push(new_sub_arr) if new_sub_arr.size > 0
+    new_sub_arr = []
+    p new_sub_arr.push(e)
+  else
+    # just add to sub arr
+    p new_sub_arr.push(e)
+  end
+  
+  new_arr.push(new_sub_arr) if i == (arr.size - 1)
+end
+
+p new_arr
