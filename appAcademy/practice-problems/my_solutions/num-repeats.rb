730a32216defcac120546291c8ea5c5b8a819848
Femke Knottenbelt <femknotgithub@gmail.com>
2018-02-02 21:43:33 +0000

    Solved appAc problem 20

---
+++ appAcademy/practice-problems/my_solutions/num-repeats.rb
@@ -0,0 +1,30 @@
+# Write a method that takes in a string and returns the number of
+# letters that appear more than once in the string. You may assume
+# the string contains only lowercase letters. Count the number of
+# letters that repeat, not the number of times they repeat in the
+# string.
+#
+# Difficulty: hard.
+
+def num_repeats(string)
+  arr = string.split("")
+  repeat_arr=[]
+  arr.each do |letter|
+    if arr.count(letter) > 1
+      if repeat_arr.include?(letter) == false
+        repeat_arr.push(letter)
+      end
+    end  
+  end
+  repeats = repeat_arr.size
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts('num_repeats("abdbc") == 1: ' + (num_repeats('abdbc') == 1).to_s)
+# one character is repeated
+puts('num_repeats("aaa") == 1: ' + (num_repeats('aaa') == 1).to_s)
+puts('num_repeats("abab") == 2: ' + (num_repeats('abab') == 2).to_s)
+puts('num_repeats("cadac") == 2: ' + (num_repeats('cadac') == 2).to_s)
+puts('num_repeats("abcde") == 0: ' + (num_repeats('abcde') == 0).to_s)
