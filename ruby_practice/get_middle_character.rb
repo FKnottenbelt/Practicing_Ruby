7b5c6b91a51151bf0465b8a4cf9a442cb8c206f4
Femke Knottenbelt <femknotgithub@gmail.com>
2018-05-09 07:02:43 +0000

    Add problem Get Middle Character

---
+++ ruby_practice/get_middle_character.rb
@@ -0,0 +1,44 @@
+# get_middle_character.rb 7 kyu
+
+=begin
+You are going to be given a word. Your job is to return the middle character
+of the word. If the word's length is odd, return the middle character. If the
+word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+
+#Input
+
+A word (string) of length 0 < str < 1000 (In javascript you may get slightly
+more than 1000 in some test cases due to an error in the test cases). You do
+not need to test for this. This is only here to tell you that you do not need
+to worry about your solution timing out.
+
+#Output
+
+The middle character(s) of the word represented as a string.
+===
+=end
+
+def get_middle(s)
+  s.size.odd? ? s[s.size / 2] : s[(s.size / 2) - 1] + s[s.size / 2]
+end
+
+p get_middle("test") == "es"
+p get_middle("testing") =="t"
+p get_middle("middle") == "dd"
+p get_middle("A") == "A"
+p get_middle("of") == "of"
+
+# top solution
+def get_middle(s)
+  s[(s.size-1)/2..s.size/2]
+end
