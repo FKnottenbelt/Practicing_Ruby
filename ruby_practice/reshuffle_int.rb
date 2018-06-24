abbd38e727dcc9aa5b8304854a8cb2372401ab2e
Femke Knottenbelt <femknotgithub@gmail.com>
2018-06-24 13:37:47 +0000

    Add problem Reshuffle Integer

---
+++ ruby_practice/reshuffle_int.rb
@@ -0,0 +1,40 @@
+# Given a number, return the highest number that could be
+# formed from the input number's digits.
+
+
+puts max_number(213) == 321
+puts max_number(7389) == 9873
+puts max_number(63792) == 97632
+puts max_number(566797) == 977665
+puts max_number(1000000) == 1000000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################333
+=begin
+I: int
+o: resuffeld int to highest number
+f: break it apart, sort desc, make back to int
+   make int to digits
+   sort and reverse
+   join
+   make int
+=end
+
+def max_number(int)
+  int.digits.sort.reverse.join.to_i
+end
+
