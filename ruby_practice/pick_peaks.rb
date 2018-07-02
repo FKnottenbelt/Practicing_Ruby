821c1e20c16793222b228322d6066863fc9c8bc6
Femke Knottenbelt <femknotgithub@gmail.com>
2018-07-02 20:19:17 +0000

    Review

--- ruby_practice/pick_peaks.rb
+++ ruby_practice/pick_peaks.rb
@@ -1,28 +1,28 @@
 # Pick peaks
 
-# In this kata, you will write a function that returns the positions and the values 
+# In this kata, you will write a function that returns the positions and the values
 # of the "peaks" (or local maxima) of a numeric array.
 
-# For example, the array arr = [0, 1, 2, 5, 1, 0] has a peak at position 3 with 
+# For example, the array arr = [0, 1, 2, 5, 1, 0] has a peak at position 3 with
 # a value of 5 (since arr[3] equals 5).
 
-# The output will be returned as an object with two properties: pos and peaks. 
-# Both of these properties should be arrays. If there is no peak in the given array, 
+# The output will be returned as an object with two properties: pos and peaks.
+# Both of these properties should be arrays. If there is no peak in the given array,
 # then the output should be {pos: [], peaks: []}.
 
-# Example: pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]) should 
+# Example: pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]) should
 # return {pos: [3, 7], peaks: [6, 3]} (or equivalent in other languages)
 
-# All input arrays will be valid integer arrays (although it could still be empty), 
+# All input arrays will be valid integer arrays (although it could still be empty),
 # so you won't need to validate the input.
 
-# The first and last elements of the array will not be considered as peaks 
+# The first and last elements of the array will not be considered as peaks
 # (in the context of a mathematical function, we don't know what is after and
 # before and therefore, we don't know if it is a peak or not).
 
-# Also, beware of plateaus !!! [1, 2, 2, 2, 1] has a peak while [1, 2, 2, 2, 3] 
-# does not. In case of a plateau-peak, please only return the position 
-# and value of the beginning of the plateau. For example: pickPeaks([1, 2, 2, 2, 1]) 
+# Also, beware of plateaus !!! [1, 2, 2, 2, 1] has a peak while [1, 2, 2, 2, 3]
+# does not. In case of a plateau-peak, please only return the position
+# and value of the beginning of the plateau. For example: pickPeaks([1, 2, 2, 2, 1])
 # returns {pos: [1], peaks: [2]} (or equivalent in other languages)
 ####
 # i: array of numbers, could be empty
@@ -35,7 +35,7 @@
 #   - but if the array.lenght < 3 don't bother and return empty arrays (make that the default)
 # f: - check length of array and if < 3 return default
 #   - loop from index 2 to index len -2
-#       - compare value with value of index -1. 
+#       - compare value with value of index -1.
 #         if bigger:
 #           - compare value with value of index + 1
 #           if bigger -> peak , write to hash
@@ -44,13 +44,13 @@
 
 #### NB: passes test but not on codewars. I think because the output needs to be an object
 # with 2 properties. Which I don't know how to do yet...
-    
+
 def pick_peaks array
   len = array.length
   output = {"pos"=>[], "peaks"=> []}
-  
+
   return output if len < 3
-   
+
   for i in 2..(len-2)
     if array[i] > array[i-1]  && (array[i] > array[i+1] ||
       (array[i] ==  array[i+1]  &&  array[i] > array[len-1]))
@@ -62,7 +62,7 @@ def pick_peaks array
   end
   output
 end
-      
+
 
 p pick_peaks([1,2,3,6,4,1,2,3,2,1]) == {"pos"=>[3,7], "peaks"=>[6,3]}
 p pick_peaks([3,2,3,6,4,1,2,3,2,1,2,3])== {"pos"=>[3,7], "peaks"=>[6,3]}
