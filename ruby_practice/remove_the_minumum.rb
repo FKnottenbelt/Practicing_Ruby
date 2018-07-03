36a65fd15e81e8933a22bed08c0f6543a704e4ea
Femke Knottenbelt <femknotgithub@gmail.com>
2018-07-03 20:03:36 +0000

    Review

--- ruby_practice/remove_the_minumum.rb
+++ ruby_practice/remove_the_minumum.rb
@@ -23,16 +23,26 @@
 # remove_smallest([5,3,2,1,4]) = [5,3,2,4]
 # remove_smallest([2,2,1,2,1]) = [2,2,2,1]
 
+p remove_smallest([1,2,3,4,5]) == [2,3,4,5]
+p remove_smallest([5,3,2,1,4]) == [5,3,2,4]
+p remove_smallest([2,2,1,2,1]) == [2,2,2,1]
+p remove_smallest([]) == []
 
+
+
+########
 def remove_smallest(numbers)
   i = numbers.index(numbers.sort.first)
   numbers.delete_at(i) if i != nil
   numbers
 end
 
-p remove_smallest([1,2,3,4,5]) == [2,3,4,5]
-p remove_smallest([5,3,2,1,4]) == [5,3,2,4]
-p remove_smallest([2,2,1,2,1]) == [2,2,2,1]
+# alternative
+def remove_smallest(numbers)
+  return [] if numbers.empty?
+  numbers.slice!(numbers.index(numbers.min))
+  numbers
+end
 
 # top solutions
 
